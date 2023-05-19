73bb3867297ce1716e6a6498deebe136be3c384c
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-19 15:44:01 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
 
+int funzione(int numeri[], int i)
+{
+    int x=1, max=numeri[0];
+    while (x<i)
+    {
+        if(numeri[x]>max)
+        {
+            max=numeri[x];
+        }
+        x++;
+    }
+    return max;
+}
