8bf8f7c034bf4f0345285785f0c9b50d2bdcb87a
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-19 17:34:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ int main() {
 int i;
 int a;
 int numeri[i];
+std::cin >> i;
+int x=1, max=numeri[0];
+    for (int a=0; a<i; a++)
+    {
+        std::cin >> numeri[a];
+    }
+
 std::cout << funzione(numeri, i);
 return 0;
 }
