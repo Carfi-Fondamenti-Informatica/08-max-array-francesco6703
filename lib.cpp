b07651afccfe361127c91422e4f6a9edef2d776c
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-19 17:33:46 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,7 @@
-#include <iostream>
 #include "lib.h"
 
 int funzione(int numeri[], int i)
 {
-
-    std::cin >> i;
-    int x=1, max=numeri[0];
-    for (int a=0; a<i; a++)
-    {
-        std::cin >> numeri[a];
-    }
-
     while (x<i)
     {
         if(numeri[x]>max)
