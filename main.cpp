92f3ca8cb4bb9eb1ebc91bb9a3855688160218a9
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-19 18:12:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "lib.h"
 
+
 int main() {
-int i=0;
-int a=0;
-int max;
-int numeri[a];
-max = funzione(numeri, i);
-std::cout << max;
-return 0;
+    int i=0;
+    int a=0;
+    int max;
+    int numeri[a];
+    max = funzione(numeri, i);
+    std::cout << max;
+    return 0;
 }
