a61a23130fc5c6aad68e2662a0d4121882a432f8
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-20 16:16:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
 int main() {
-    int i = 0;
-    int a;
+    int i=0;
+    int a=0;
+    int max;
     int numeri[a];
-    do {
-        cin >> i;
-    }while (i<1);
-
-    for (a=0; a<i; a++) {
-        cin >> numeri[a];
-    }
-    cout << funzione(numeri, i);
+    max = funzione(numeri, i);
+    cout << max;
     return 0;
 }
