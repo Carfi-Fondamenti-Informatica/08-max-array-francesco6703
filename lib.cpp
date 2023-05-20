b2d74a9bb91169bf21bb2b8f3d039e22b1a0828b
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-20 16:02:41 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,12 @@
 #include "lib.h"
-int funzione(int numeri[], int i)
-{
+
+int funzione(int *numeri, int i) {
     int b=1;
-    int max=numeri[0];
+    float max=numeri[0];
 
     while (b<i)
     {
-        if(numeri[b]>max)
+        if(numeri[b]>=max)
         {
             max=numeri[b];
         }
